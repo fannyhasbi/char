6ef170fcd86262d1470d907c76cd04a4e58a2d7c
Fanny Hasbi <fnyhsbi@gmail.com>
2018-02-05 22:26:51 +0700

    Add id_session for cart

--- apz/controllers/Home.php
+++ apz/controllers/Home.php
@@ -2,6 +2,26 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller {
+  public function __construct(){
+    parent::__construct();
+    $this->cekSession();
+  }
+
+  //cek apakah session sudah ada
+  private function cekSession(){
+    if(!$this->session->userdata('id_session'))
+      $this->session->set_userdata(array('id_session' => $this->generateSession()));
+  }
+
+  private function generateSession(){
+    $h = "";
+    $k = "1234567890qwertyuiopasdfghjklzxcvbnm";
+    for ($i=1; $i <= 40; $i++) { 
+      $h .= $k[rand(0, strlen($k) - 1)];
+    }
+    return $h;
+  }
+
   public function index(){
     $this->load->view('home/index');
   }
